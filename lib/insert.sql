6f9d19333f847f8e54d1a54e3172774c3fe400c7
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-10-21 17:44:11 -0400

    bears

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+--  id INTEGER PRIMARY KEY,
+--     name TEXT, 
+--     age INTEGER,
+--     gender TEXT,
+--     color TEXT,
+--     temperament TEXT,
+--     alive BOOLEAN
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "Brown", "v. serious", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 21, "F", "Fuschia", "party bear", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "F", "brown", "angry", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "F", "black", "kind", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 36, "F", "gray", "vengeful", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 18, "M", "green", "grouchy", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 15, "F", "black", "kind", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "M", "brown", "angry", FALSE);
